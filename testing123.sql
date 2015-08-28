77436f5cbc21d88e9fd994e6a4830aba4b07ca98
Akiko Iwamizu <akikoiwamizu@gmail.com>
2015-08-27 21:39:35 -0700

    Joining actor, film id, and film tables

--- testing123.sql
+++ testing123.sql
@@ -1,1 +1,5 @@
-SELECT * FROM sakila.actor;
+SELECT * 
+FROM sakila.actor AS sa, sakila.film_actor AS sfa, sakila.film AS sf
+WHERE sa.actor_id = sfa.actor_id
+AND sfa.film_id = sf.film_id
+-- GROUP BY sa.actor_id;
