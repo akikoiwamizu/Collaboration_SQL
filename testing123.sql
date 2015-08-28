4df4afcfbb8b4e8d4d958aa6b33a81841a9943e0
Akiko Iwamizu <akikoiwamizu@gmail.com>
2015-08-27 21:48:54 -0700

    Cleaning up

--- testing123.sql
+++ testing123.sql
@@ -1,5 +1,11 @@
 SELECT * 
-FROM sakila.actor AS sa, sakila.film_actor AS sfa, sakila.film AS sf
+FROM sakila.actor AS sa, 
+	sakila.film_actor AS sfa, 
+    sakila.film AS sf,
+    sakila.inventory AS si
 WHERE sa.actor_id = sfa.actor_id
 AND sfa.film_id = sf.film_id
--- GROUP BY sa.actor_id;
+AND sfa.film_id = si.film_id
+AND sf.rating = "R"
+GROUP BY sfa.film_id
+ORDER BY sf.rental_rate DESC;
